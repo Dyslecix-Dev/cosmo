51ca9227641391d9ebabb85097ae3912ad7d7525
Christian Demesa <dyslecixdev@gmail.com>
2026-04-22 20:39:33 -0700

    feat: scaffold root configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+
   // TODO: change site
   site: "https://yourdomain.com",
 
