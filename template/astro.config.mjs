bcdb6db6fe6fb93d8dfcb3ae5429d59bfffdc704
Christian Demesa <dyslecixdev@gmail.com>
2026-04-25 13:44:19 -0700

    Revert "Merge pull request #9 from Dyslecix-Dev/cloudflare/workers-autoconfig"
    This reverts commit 93c32c47f7c1cce368dde060fd969d134e541078, reversing
    changes made to 9cbb74f1e807efe201545f126728a8c73a80d421.

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -5,8 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, fontProviders } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   output: "static",
 
@@ -29,11 +27,10 @@ export default defineConfig({
   ],
 
   site: "https://cosmo.dyslecix.dev",
+
   integrations: [mdx(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare(),
 });
