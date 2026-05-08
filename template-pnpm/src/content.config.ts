30bd9991d610a6a5cb79e9a0968d095dbb9a7e09
Christian Demesa <dyslecixdev@gmail.com>
2026-05-07 20:42:12 -0700

    chore: remove comments

--- template-pnpm/src/content.config.ts
+++ template-pnpm/src/content.config.ts
@@ -2,12 +2,6 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
-/**
- * NOTE: example content collection. Delete or adapt for your use case:
- *  - blog: chronological posts with tags + hero images
- *
- * See https://docs.astro.build/en/guides/content-collections/
- */
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
   schema: ({ image }) =>
